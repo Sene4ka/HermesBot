fc2f5203b907c1f783ee953a2b7478e2b2f911f8
Арсений <sene4ka.abrosimov@yandex.ru>
2021-04-23 12:05:23 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODEyOTk2NjgyNDkzMDY3Mjk1.YDI4KQ.CR7tvVrJUCQ0MXxb42CHVx_cMcs',
+    'token': 'TOKEN',
     'bot': 'ТИГРАН ЛОХ',
     'id': 812996682493067295,
     'prefix': '?'
